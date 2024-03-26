1d5c67f43b7ff3c3623e5066741a77cde0851153
hhtun137 <htunaunghlaing199@gmail.com>
2024-03-26 20:35:07 +0630

    using model attribute with contructor and redirect url

--- src/main/java/com/HAH/mvc/controller/StudentController.java
+++ src/main/java/com/HAH/mvc/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.HAH.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class StudentController {
 
 	@GetMapping
-	public String greeting() {
+	public String greeting(Model model) {
+		model.addAttribute("message", "This message from student controller.");
 		return "index";
 	}
 }
