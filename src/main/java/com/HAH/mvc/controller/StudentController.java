907810921b350bcfe8bff653a36c526eed5f236d
hhtun137 <htunaunghlaing199@gmail.com>
2024-03-27 13:02:20 +0630

    config & use root application context

--- src/main/java/com/HAH/mvc/controller/StudentController.java
+++ src/main/java/com/HAH/mvc/controller/StudentController.java
@@ -1,17 +1,23 @@
 package com.HAH.mvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.HAH.mvc.service.StudentService;
+
 @Controller
 @RequestMapping("/hello")
 public class StudentController {
 
+	@Autowired
+	private StudentService service;
+
 	@GetMapping
 	public String greeting(Model model) {
-		model.addAttribute("message", "This message from student controller.");
+		model.addAttribute("message", service.getMessage());
 		return "index";
 	}
 }
---
+++ src/main/java/com/HAH/mvc/service/StudentService.java
@@ -0,0 +1,12 @@
+package com.HAH.mvc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+	public String getMessage() {
+		return "This message from Root Application Context";
+	}
+
+}
